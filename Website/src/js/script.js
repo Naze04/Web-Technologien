dbe6a13b7e4373bc56ffa315028fe7d047b9fef1
hamdepaf <hamhamdepaf@gmx.de>
2025-01-09 15:46:35 +0100

    Structure tweak + Robin's about us page

--- Website/src/js/script.js
+++ Website/src/js/script.js
@@ -13,7 +13,15 @@ function loadNav() {
                         <li><a href="PLACEHOLDER"><h5>4</h5></a></li>\
                     </ul>\
                 </li>\
-                <li><a href="OPhone.html"><h4>OPhone</h4></a></li>\
+                <li>\
+                    <a href="OPhone.html"><h4>OPhone</h4></a>\
+                    <ul class="dropdown">\
+                        <li><a href="PLACEHOLDER"><h5>1</h5></a></li>\
+                        <li><a href="PLACEHOLDER"><h5>2</h5></a></li>\
+                        <li><a href="PLACEHOLDER"><h5>3</h5></a></li>\
+                        <li><a href="PLACEHOLDER"><h5>4</h5></a></li>\
+                    </ul>\
+                </li>\
                 <li><a href="OWatch.html"><h4>OWatch</h4></a></li>\
                 <li><a href="EarthPods.html"><h4>EarthPods</h4></a></li>\
             </ul>'
