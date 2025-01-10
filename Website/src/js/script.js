d35668bba08ab014f528479a34673c74aa29cff4
hamdepaf <hamhamdepaf@gmx.de>
2025-01-10 18:58:06 +0100

    splitting up the style.css and renaming some files

--- Website/src/js/script.js
+++ Website/src/js/script.js
@@ -13,8 +13,9 @@ function loadNav() {
                         <li><a href="PLACEHOLDER"><h5>4</h5></a></li>\
                     </ul>\
                 </li>\
+                <li><a href="shop.html"><h4>Shop</h4></a></li>\
                 <li>\
-                    <a href="OPhone.html"><h4>OPhone</h4></a>\
+                    <a href="oPhone.html"><h4>OPhone</h4></a>\
                     <ul class="dropdown">\
                         <li><a href="PLACEHOLDER"><h5>1</h5></a></li>\
                         <li><a href="PLACEHOLDER"><h5>2</h5></a></li>\
@@ -22,7 +23,8 @@ function loadNav() {
                         <li><a href="PLACEHOLDER"><h5>4</h5></a></li>\
                     </ul>\
                 </li>\
-                <li><a href="OWatch.html"><h4>OWatch</h4></a></li>\
-                <li><a href="EarthPods.html"><h4>EarthPods</h4></a></li>\
+                <li><a href="oWatch.html"><h4>OWatch</h4></a></li>\
+                <li><a href="earthPods.html"><h4>EarthPods</h4></a></li>\
+                <li><a href="aboutUs.html"><h4>About Us</h4></a></li>\
             </ul>'
 }
