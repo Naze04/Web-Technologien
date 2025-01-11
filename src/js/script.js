3d6c16e9e4062a136e10d27ffa64bd54e0a73f3d
hamdepaf <hamhamdepaf@gmx.de>
2025-01-11 18:28:42 +0100

    moved files + added sebastians checkout.html (propper link to it needed)

--- src/js/script.js
+++ src/js/script.js
@@ -7,13 +7,19 @@ function loadNav() {
                 <li>\
                     <a href="home.html"><img alt="Logo" src=../assets/orange_logo.png class="logo"></a>\
                     <ul class="dropdown">\
-                        <li><a href="PLACEHOLDER"><h5>1</h5></a></li>\
+                        <li><a href="PLACEHOLDER"><h5>test</h5></a></li>\
                         <li><a href="PLACEHOLDER"><h5>2</h5></a></li>\
                         <li><a href="PLACEHOLDER"><h5>3</h5></a></li>\
                         <li><a href="PLACEHOLDER"><h5>4</h5></a></li>\
                     </ul>\
                 </li>\
                 <li><a href="shop.html"><h4>Shop</h4></a></li>\
+                    <ul class="dropdown">\
+                        <li><a href="PLACEHOLDER"><h5>1</h5></a></li>\
+                        <li><a href="PLACEHOLDER"><h5>2</h5></a></li>\
+                        <li><a href="PLACEHOLDER"><h5>3</h5></a></li>\
+                        <li><a href="PLACEHOLDER"><h5>4</h5></a></li>\
+                    </ul>\
                 <li>\
                     <a href="oPhone.html"><h4>OPhone</h4></a>\
                     <ul class="dropdown">\
