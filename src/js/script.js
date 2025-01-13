052f25ec6f4fad934d78ac59c15233ebee582933
Naze <simonreckinger@gmx.de>
2025-01-13 12:03:54 +0100

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -34,3 +34,40 @@ function loadNav() {
                 <li><a href="aboutUs.html"><h4>About Us</h4></a></li>\
             </ul>'
 }
+
+//Cardslider
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const nextBtn = document.querySelector('#next-btn');
+        const prevBtn = document.querySelector('#prev-btn');
+        const slider = document.querySelector('#slider');
+        if (nextBtn) {
+            console.log('Next button found:', nextBtn);
+        } else {
+            console.error('Next button not found');
+        }
+    
+        let currentIndex = 0;
+    
+        nextBtn.addEventListener('click', () => {
+          const cards = document.querySelectorAll('.card');
+          const cardWidth = cards.length > 0 ? cards[0].offsetWidth + 20 : 0;
+          const maxIndex = cards.length - Math.floor(slider.offsetWidth / cardWidth);
+          
+          if (currentIndex < maxIndex) {
+            currentIndex++;
+            slider.style.transform = `translateX(-${cardWidth * currentIndex}px)`;
+          }
+        });
+    
+        prevBtn.addEventListener('click', () => {
+          const cards = document.querySelectorAll('.card');
+          const cardWidth = cards[0].offsetWidth + 20;
+    
+          if (currentIndex > 0) {
+            currentIndex--;
+            slider.style.transform = `translateX(-${cardWidth * currentIndex}px)`;
+          }
+        });
+    });
+
