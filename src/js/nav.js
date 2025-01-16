f63b5821392945a9b38255daf0a1fd3dfc00e946
hamdepaf <hamhamdepaf@gmx.de>
2025-01-16 16:48:09 +0100

    login finish

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,12 @@
 function onLoginClick() {
     let element = document.getElementById("logindropdown")
-    element.setAttribute("clip", "auto")
-    element.setAttribute("opacity", 1)
-    element.style.transform = "scaleY(1)"
+    if (element.isOpen != true) {
+        element.style.opacity = 1
+        element.style.transform = "scaleY(1)"
+        element.isOpen = true
+    } else {
+        element.style.opacity = 0
+        element.style.transform = "scaleY(0)"
+        element.isOpen = false
+    }
 }
